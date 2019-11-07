9335ca73a1bc88a178e77d51e6a6e4cf4f056362
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-11-07 14:13:50 -0800

    added scheme-model.js

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,29 +1,49 @@
-// find():
-// Calling find returns a promise that resolves to an array of all schemes in the database.
-// No steps are included.
+const db = require('../data/dbConfig.js');
 
-// findById(id):
-// Expects a scheme id as its only parameter.
-// Resolve to a single scheme object.
-// On an invalid id, resolves to null.
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+};
 
-// findSteps(id):
-// Expects a scheme id.
-// Resolves to an array of all correctly ordered step for the given scheme: [ { id: 17, scheme_name: 'Find the Holy Grail', step_number: 1, instructions: 'quest'}, { id: 18, scheme_name: 'Find the Holy Grail', step_number: 2, instructions: '...and quest'}, etc. ].
-// This array should include the scheme_name not the scheme_id.
+function get() {
+  return db('schemes');
+}
 
-// add(scheme):
-// Expects a scheme object.
-// Inserts scheme into the database.
-// Resolves to the newly inserted scheme, including id.
+function findById(id) {
+  return db('schemes')
+    .where({ id })
+    .first();
+    //invalid resolves to null
+}
 
-// update(changes, id):
-// Expects a changes object and an id.
-// Updates the scheme with the given id.
-// Resolves to the newly updated scheme object.
+function findSteps(id) {
+    return db('schemes')
+      .where({ id })
+      .first();
+//Resolves to an array of all correctly ordered step for the given scheme: [ { id: 17, scheme_name: 'Find the Holy Grail', step_number: 1, instructions: 'quest'}, { id: 18, scheme_name: 'Find the Holy Grail', step_number: 2, instructions: '...and quest'}, etc. ].
+//This array should include the scheme_name not the scheme_id.
+  }
 
-// remove(id):
-// Removes the scheme object with the provided id.
-// Resolves to the removed scheme
-// Resolves to null on an invalid id.
-(Hint: Only worry about removing the scheme. The database is configured to automatically remove all associated steps.)
+function add(scheme) {
+  return db('schemes')
+    .insert(scheme)
+    .then(ids => {
+      return findById(ids[0]);
+    });
+}
+
+function update(changes, id) {
+  return db('schemes')
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db('schemes')
+    .where('id', id)
+    .del();
+}
